a1d7d1e0f55bd2e0b0642440a7b8996f9d963b64
coffebean19 <istvan.mayer1999@protonmail.com>
2024-01-17 17:45:57 +0200

    Section 2p15.

--- course-starting-project/course-starting-project/app.ts
+++ course-starting-project/course-starting-project/app.ts
@@ -1,1 +1,20 @@
-console.log('Your code goes here...');
+console.log('Time to get begin...');
+
+function add(n1: number, n2: number, result: boolean, phrase: string) {
+  // if (typeof n1 !== 'number' || typeof n2 == 'number') {
+  // throw new Error("Incorrect input!");
+  // }
+  const num = n1 + n2;
+  if (result) {
+    console.log(phrase + num)
+  } else {
+    return num;
+  }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+add(number1, number2, printResult, resultPhrase);
