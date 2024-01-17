32cea521f9a26171c90b62983053cb471a203160
coffebean19 <istvan.mayer1999@protonmail.com>
2024-01-17 18:48:44 +0200

    Section 2p18. Types and Objects, and Arrays.

--- course-starting-project/course-starting-project/app.ts
+++ course-starting-project/course-starting-project/app.ts
@@ -1,20 +1,14 @@
-console.log('Time to get begin...');
+// const person: {
+//   name: string,
+//   age: number
+// } = {
+const person = {
+  name: "Istvan",
+  age: 24,
+  hobbies: ["Gaming", "Reading"]
+};
 
-function add(n1: number, n2: number, result: boolean, phrase: string) {
-  // if (typeof n1 !== 'number' || typeof n2 == 'number') {
-  // throw new Error("Incorrect input!");
-  // }
-  const num = n1 + n2;
-  if (result) {
-    console.log(phrase + num)
-  } else {
-    return num;
-  }
-}
+let favActs: string[]; //any[]
+favActs = ["Sports"];
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "Result is: ";
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person);
