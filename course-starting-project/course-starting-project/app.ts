9031d6e857bfb3a91806c90ce15775c5e940465a
coffebean19 <istvan.mayer1999@protonmail.com>
2024-01-18 17:24:38 +0200

    Section 2p21.

--- course-starting-project/course-starting-project/app.ts
+++ course-starting-project/course-starting-project/app.ts
@@ -2,10 +2,13 @@
 //   name: string,
 //   age: number
 // } = {
+enum Role { ADMIN, READ_ONLY_USER, AUTHOR };
+
 const person = {
   name: "Istvan",
   age: 24,
-  hobbies: ["Gaming", "Reading"]
+  hobbies: ["Gaming", "Reading"],
+  role: Role.ADMIN
 };
 
 let favActs: string[]; //any[]
