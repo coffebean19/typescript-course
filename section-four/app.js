66faff4085468938b4c04fb413c16889d0d2e498
coffebean19 <istvan.mayer1999@protonmail.com>
2024-02-06 12:51:57 +0200

    Finished Section 4.

---
+++ section-four/app.js
@@ -0,0 +1,30 @@
+"use strict";
+// const add = (a: number, b: number = 1) => a + b;
+// console.log(add(4, 3));
+// console.log(add(5));
+const printOut = output => console.log(output);
+const hobbies = ["Hiking", "Reading"];
+const activeHobbies = ["Writing"];
+activeHobbies.push(...hobbies);
+const person = {
+    firstName: "John",
+    age: 80,
+};
+// const copiedPerson = person;
+// person.name = "Pop";
+// console.log(copiedPerson.name)
+const copiedPerson = Object.assign({}, person);
+person.firstName = "Other";
+console.log(copiedPerson.firstName);
+const add = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        // console.log("CurValue:", curResult)
+        // console.log("CurResult: ", curResult);
+        return curResult + curValue;
+    }, 0);
+};
+console.log(add(1, 2, 5, 8));
+const [hobby1, hobby2, ...remainHobies] = hobbies;
+console.log(hobby1);
+const { firstName, age } = person;
+console.log(firstName);
--- section-four/app.ts
+++ section-four/app.ts
@@ -1,7 +1,40 @@
-const add = (a: number, b: number) => a + b;
+// const add = (a: number, b: number = 1) => a + b;
 
-console.log(add(4, 3));
+// console.log(add(4, 3));
+// console.log(add(5));
 
 const printOut: (a: string | number) => void = output => console.log(output);
 
-printOut(3)
+const hobbies = ["Hiking", "Reading"]
+const activeHobbies = ["Writing"]
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  firstName: "John",
+  age: 80,
+}
+
+// const copiedPerson = person;
+// person.name = "Pop";
+// console.log(copiedPerson.name)
+
+const copiedPerson = { ...person };
+person.firstName = "Other";
+console.log(copiedPerson.firstName);
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    // console.log("CurValue:", curResult)
+    // console.log("CurResult: ", curResult);
+    return curResult + curValue;
+  }, 0);
+}
+
+console.log(add(1, 2, 5, 8));
+
+const [hobby1, hobby2, ...remainHobies] = hobbies;
+console.log(hobby1);
+
+const { firstName, age } = person;
+console.log(firstName);
