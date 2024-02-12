2142987e2762467fe42dbda9b47e36e9321091b0
coffebean19 <istvan.mayer1999@protonmail.com>
2024-02-12 15:44:03 +0200

    Section 5p71 Finish. Quiz 5 completed.

--- section-five/src/app.js
+++ section-five/src/app.js
@@ -1,12 +1,14 @@
 "use strict";
 class Department {
+    static createEmployee(name) {
+        return {
+            name: name
+        };
+    }
     constructor(id, name) {
         this.id = id;
         this.name = name;
         this.employees = [];
-    }
-    describe() {
-        console.log("Department: " + this.name);
     }
     addEmployee(employee) {
         this.employees.push(employee);
@@ -16,9 +18,69 @@ class Department {
         console.log(this.employees);
     }
 }
-const accounting = new Department("1", "Accounting");
-// console.log(accounting);
-accounting.describe();
-accounting.addEmployee("Istvan");
-accounting.addEmployee("Rolande");
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super(id, "IT Department");
+        this.admins = admins;
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    addAdmin(admin) {
+        this.admins.push(admin);
+    }
+    describeAdmins() {
+        console.log(this.admins);
+    }
+    describe() {
+        console.log("THis is the IT department");
+    }
+}
+class AccountingDepartment extends Department {
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        else {
+            throw new Error("No Reports");
+        }
+    }
+    describe() {
+        console.log("Somespecil");
+    }
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error("No report provided");
+        }
+        else {
+            this.addReport(value);
+        }
+    }
+    constructor(id, reports) {
+        super(id, "Accounting Department");
+        this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    addReport(report) {
+        this.reports.push(report);
+        this.lastReport = this.reports[0];
+    }
+    describeReports() {
+        console.log(this.reports);
+    }
+}
+const employee1 = Department.createEmployee("Gus");
+// const department = new Department("1", "Accounting");
+// // console.log(accounting);
+// department.describe();
+// department.addEmployee("Istvan");
+// department.addEmployee("Rolande");
+// department.describeEmployees();
+const it = new ITDepartment("id2", ["Istvan"]);
+it.describeAdmins();
+const accounting = new AccountingDepartment("id3", []);
+accounting.addReport("Oh no");
+accounting.describeReports();
+accounting.addEmployee("Person rtwo");
 accounting.describeEmployees();
+accounting.mostRecentReport = "someting";
